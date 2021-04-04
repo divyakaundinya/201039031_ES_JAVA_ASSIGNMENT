620d8a667fe7f098235da7c8112be74c53a96782
divyakaundinya <73835793+divyakaundinya@users.noreply.github.com>
2021-04-04 16:27:31 +0530

    Add files via upload

---
+++ Sqrroot.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+
+class Sqrroot{
+	public static void main(String args[]){
+		double number = Double.parseDouble(args[0]);
+		System.out.println("The square root of the given number is : "+ Math.sqrt(number));		
+	}
+}
